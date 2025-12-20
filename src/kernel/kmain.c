4db2233670165b935bf87f8dba695befa4cf7171
maiet <prolib69420@gmail.com>
2025-12-20 10:09:39 -0500

    cleaned up kmain

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -1,9 +1,6 @@
 #include "cpu_utils.h"
 #include "framebuffer.h"
 #include "limine.h"
-#include "moneycat.h"
-#include "font.h"
-#include "printf.h"
 #include "gdt.h"
 #include "idt.h"
 #include "umain.h"
@@ -16,21 +13,6 @@ void kmain(void)
 
     if (!init_framebuffer())
         CU_halt();
-
-    /*uint32_t *fb = get_fb();
-    size_t fb_width = get_fb_width();
-    size_t fb_height = get_fb_height();
-    size_t fb_pitch = get_fb_pitch();
-
-#ifdef DEBUG_BUILD
-    bool is_debug = true;
-#else
-    bool is_debug = false;
-#endif
-
-    char buf[256];
-    snprintf(buf, sizeof(buf), "FB Resolution: %dx%d, is_debug: %d", fb_width, fb_height, is_debug);
-    draw_string(fb, fb_pitch, 1, 1, buf, 0xFFFFFF);*/
 
     umain();
 
